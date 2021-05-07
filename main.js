22f7aaaa239d3a290fb4943aae22327eac2e2ca8
Irvin <61360847+irvin-s@users.noreply.github.com>
2021-05-06 23:58:46 -0300

    Pck: Adding Crypto.js package

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 const SHA256 = require('crypto-js/sha256');
 
-class Block{
-    constructor(index, timestamp, data, previousHash = ''){
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
-    calculateHash(){
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 
     }
 
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2017", "Genesis Block", "0");
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLastBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+}
+
+let irvinCoin = new Blockchain();
+irvinCoin.addBlock(new Block(1, "06/05/2021", { amount: 4 }));
+irvinCoin.addBlock(new Block(2, "07/05/2021", { amount: 8 }));
+
+console.log(JSON.stringify(irvinCoin, null, 4));
