a56bdc4aa41bd4a4dd312ecb52b02c9164f7b645
Irvin <61360847+irvin-s@users.noreply.github.com>
2021-06-08 23:15:41 -0300

    Adding prof-of-work

--- main.js
+++ main.js
@@ -7,18 +7,27 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
 
     }
 
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
+    }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 6;
     }
 
     createGenesisBlock() {
@@ -31,7 +40,7 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLastBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -56,12 +65,18 @@ class Blockchain {
 }
 
 let irvinCoin = new Blockchain();
+
+console.log('Mining block 1..');
 irvinCoin.addBlock(new Block(1, "06/05/2021", { amount: 4 }));
+
+console.log('Minig block 2...');
 irvinCoin.addBlock(new Block(2, "07/05/2021", { amount: 8 }));
+
+console.log('Minig block 3...');
 irvinCoin.addBlock(new Block(3, "08/05/2021", { amount: 16 }));
 
-console.log('Is Blockchain Valid? ' + irvinCoin.isChainValid());
+/*console.log('Is Blockchain Valid? ' + irvinCoin.isChainValid());
 irvinCoin.chain[1].data = { amount: 90 };
 console.log('Is Blockchain Valid? ' + irvinCoin.isChainValid());
 
-//console.log(JSON.stringify(irvinCoin, null, 4));
+console.log(JSON.stringify(irvinCoin, null, 4));*/
